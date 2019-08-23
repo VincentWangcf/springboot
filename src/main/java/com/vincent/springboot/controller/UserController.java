7cff57a16c7ec52d50cc4b303a1ba98c923cc845
VincentWangcf <33830488+VincentWangcf@users.noreply.github.com>
2019-08-23 09:59:41 +0800

    Update UserController.java

--- src/main/java/com/vincent/springboot/controller/UserController.java
+++ src/main/java/com/vincent/springboot/controller/UserController.java
@@ -14,7 +14,7 @@ public class UserController {
 
     @RequestMapping(value = "/hello")
     public User cs() {
-        //调用dao层   添加注释a test' 啊
+        //调用dao层   添加注释a test' 啊    test
         User user = userMapper.selectUserByName("mike");
         return user;
     }
